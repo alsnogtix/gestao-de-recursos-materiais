5425426571d46444f7ce7d77870d7ed2a720bc74
alsnogtix <112113856+alsnogtix@users.noreply.github.com>
2023-09-16 20:38:48 -0300

    remoção de arquivos desnecessários

--- idatico.js/escolar.js
+++
@@ -1,7 +0,0 @@
-function abrirTab(idTab){
-  var tabs = document.getElementsByClassName('section-tabs');
-  Array.from(tabs).forEach((tab) => {
-    tab.style.display = 'none';
-  });
-  document.getElementById(idTab).style.display = 'block';
-}
